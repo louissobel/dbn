20024bbdc7c315cc3d0cdf481fbc2f0c61e4c510
Louis Sobel <sobel@lsmbp.local>
2012-10-13 20:14:11 -0400

    added verbosity options to dbn.py

--- dbn.py
+++ dbn.py
@@ -1,23 +1,39 @@
 import sys
+from optparse import OptionParser
 
 from tokenizer import DBNTokenizer
 from parser import DBNParser
 from state import DBNInterpreterState
 import output
 
+option_parser = OptionParser()
+option_parser.add_option('-v', '--verbose', action="store_true", dest="verbose", help="verbose!", default=False)
 
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    dbn_script = open(filename).read()
+(options, args) = option_parser.parse_args()
 
-    tokenizer = DBNTokenizer()
-    parser = DBNParser()
 
-    tokens = tokenizer.tokenize(dbn_script)
-    dbn_ast = parser.parse(tokens)
+VERBOSE = options.verbose
 
-    state = DBNInterpreterState()
-    state = dbn_ast.apply(state)
-    
-    output.draw_window(state.image._image)
-    
+filename = args[0]
+dbn_script = open(filename).read()
+
+tokenizer = DBNTokenizer()
+parser = DBNParser()
+
+tokens = tokenizer.tokenize(dbn_script)
+
+if VERBOSE:
+    for token in tokens:
+        print token
+
+dbn_ast = parser.parse(tokens)
+
+if VERBOSE:
+    dbn_ast.pprint()
+
+state = DBNInterpreterState()
+state = dbn_ast.apply(state)
+
+
+
+output.draw_window(state.image._image)
