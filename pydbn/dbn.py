e389308d797754e0588c608be1038cbf789eaa9c
Louis Sobel <sobel@mit.edu>
2013-04-12 13:37:06 -0400

    little refactor

--- pydbn/dbn.py
+++ pydbn/dbn.py
@@ -17,19 +17,28 @@ option_parser.add_option('-n', '--numbers', action="store_true", dest="numbers",
 option_parser.add_option('-f', '--file', action="store", dest="filename", help="file for output", default=None)
 option_parser.add_option('-t', '--trace', action="store_true", dest="trace", help="trace interpretation", default=False)
 
+def compile_dbn(filename):
+    dbn_script = open(filename).read()
+    tokenizer = DBNTokenizer()
+    parser = DBNParser()
+    compiler = DBNCompiler()
+
+    tokens = tokenizer.tokenize(dbn_script)
+    dbn_ast = parser.parse(tokens)
+    compilation = compiler.compile(dbn_ast)
+    return compilation
+
+def run_dbn(compilation):
+    interpreter = DBNInterpreter(compilation.bytecodes)
+    builtins.load_builtins(interpreter)
+    interpreter.run()
+    return interpreter
 
 if __name__ == "__main__":
     (options, args) = option_parser.parse_args()
 
     filename = args[0]
-    dbn_script = open(filename).read()
-    tokenizer = DBNTokenizer()
-    parser = DBNParser()
-    compiler = DBNCompiler()
-    
-    tokens = tokenizer.tokenize(dbn_script)
-    dbn_ast = parser.parse(tokens)
-    compilation = compiler.compile(dbn_ast)
+    compilation = compile_dbn(filename)
 
     if options.compile:
         for i, (o, a) in enumerate(compilation.bytecodes):
