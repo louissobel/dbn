08da427ff0c7abcb67a3ea6da23ef677588c6a4d
Louis Sobel <sobel@mit.edu>
2013-04-12 13:34:43 -0400

    no need for pixel_to_coord in builtins anymore

--- pydbn/builtins.py
+++ pydbn/builtins.py
@@ -3,33 +3,28 @@ import utils
 from structures import DBNBuiltinCommand
 
 class LineCommand(DBNBuiltinCommand):
-    
+
     def __init__(self):
         DBNBuiltinCommand.__init__(self, 4)
-    
+
     def keyword(self):
         return 'Line'
-    
+
     def call(self, interpreter, blX, blY, trX, trY):
-    
-        blX = utils.pixel_to_coord(blX, 'x')
-        blY = utils.pixel_to_coord(blY, 'y')
-        trX = utils.pixel_to_coord(trX, 'x')
-        trY = utils.pixel_to_coord(trY, 'y')
-    
+
         # use list so I can reuse the iterable
         points = list(utils.bresenham_line(blX, blY, trX, trY))
-    
+
         color = interpreter.pen_color
         pixel_list = ((x, y, color) for x, y in points)
-    
+
         interpreter.image.set_pixels(pixel_list)
 
 class PaperCommand(DBNBuiltinCommand):
 
     def __init__(self):
         DBNBuiltinCommand.__init__(self, 1)
-    
+
     def keyword(self):
         return 'Paper'
     
