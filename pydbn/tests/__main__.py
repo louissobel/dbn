cb078bac31301dde046339f1b6ae83eed33ef44c
Louis Sobel <sobel@mit.edu>
2013-04-12 13:46:33 -0400

    adds integration tests

--- pydbn/tests/__main__.py
+++ pydbn/tests/__main__.py
@@ -1,10 +1,22 @@
 import unittest
 import os
+import subprocess
+import StringIO
 
 test_modules = [
     'tokenizer_tests',
     'parser_tests',
+    'integration_tests',
 ]
 
 for mod in test_modules:
-    os.system('python -m tests.%s' % mod)
+    args = ['python', '-m', 'tests.%s' % mod]
+    print "Testing %s with \"%s\"...\t" % (mod, ' '.join(args)),
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, _ = proc.communicate()
+    ret = proc.returncode
+
+    if ret == 0:
+        print "OK"
+    else:
+        print "FAIL"
---
+++ pydbn/tests/integration_tests/__main__.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+from dbn import run_dbn, compile_dbn
+import output
+
+directory = os.path.dirname(__file__)
+
+test_cases = [os.path.join(directory, d) for d in os.listdir(directory) if not (d.startswith('.') or d.startswith('_'))]
+
+# assume each subdir has code.dbn and expected.bmp
+
+
+class ImageMatchTestCase():
+  
+  def get_actual(self):
+    terp = run_dbn(compile_dbn(self.directory + '/code.dbn'))
+    
+    outFile = self.directory + '/actual.bmp'
+    output.output_png(terp, outFile)
+    return open(outFile, 'rb').read()
+    
+
+  def runTest(self):
+    actual = self.get_actual()
+    expected = open(self.directory + '/expected.bmp', 'rb').read()
+    
+    self.assertEqual(actual, expected)
+
+    # this happens if it passes
+    os.remove(self.directory + '/actual.bmp')
+    
+
+for test_case in test_cases:
+  class_name = 'ImageMatchTestCase_%s' % test_case
+  locals()[class_name] = type(class_name, (unittest.TestCase, ImageMatchTestCase), {'directory': test_case})
+
+if __name__ == "__main__":
+  unittest.main()
+  
