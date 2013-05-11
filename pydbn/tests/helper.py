38a0d2d0291d05cdd094d205063e9d0719f487f7
Louis Sobel <sobel@mit.edu>
2013-05-11 16:43:48 -0400

    test runner touch ups

--- pydbn/tests/helper.py
+++ pydbn/tests/helper.py
@@ -6,23 +6,33 @@ import StringIO
 
 def run_all_in_dir(file):
     all_tests = unittest.TestLoader().discover(os.path.dirname(file), pattern='*.py')
-    r = unittest.TextTestRunner(verbosity=2).run(all_tests)
+    r = unittest.TextTestRunner(verbosity=1).run(all_tests)
 
     if r.failures or r.errors:
         sys.exit(1)
 
-def run_modules(modules, prefix=None):
+def run_modules(modules, prefix=None, verbose=True):
+    failed = False
     for mod in modules:
         if prefix:
             mod = "%s.%s" % (prefix, mod)
 
         args = ['python', '-m', 'tests.%s' % mod]
         print "Testing %s with \"%s\"...\t" % (mod, ' '.join(args)),
+
         proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         out, _ = proc.communicate()
         ret = proc.returncode
 
+        if verbose:
+            print
+            print "\n".join("\t%s" % line for line in out.split('\n'))
+
         if ret == 0:
-            print "OK"
+            print "\033[32mOK\033[0m"
         else:
-            print "FAIL"
+            failed = True
+            print "\033[31mFAIL\033[0m"
+
+    if failed:
+        sys.exit(1)
