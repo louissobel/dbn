247296683454ac58bddeec27f7b028ad436e9cf0
Louis Sobel <sobel@mit.edu>
2013-05-12 16:58:58 -0400

    path stuff

--- pydbn/__init__.py
+++ pydbn/__init__.py
@@ -1,3 +1,9 @@
+# Add the directory of this file to the python search path
+# for imports
+import os.path
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
 import parser
 import compiler
 import interpreter
