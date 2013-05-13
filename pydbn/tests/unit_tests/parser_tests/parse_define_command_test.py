420a3c0321a20c53bdf8c92bd7f551d262327545
Louis Sobel <sobel@mit.edu>
2013-05-12 22:09:50 -0400

    rework procedure definition tests

--- pydbn/tests/unit_tests/parser_tests/parse_define_command_test.py
+++ pydbn/tests/unit_tests/parser_tests/parse_define_command_test.py
@@ -4,21 +4,22 @@ import parser
 from parser.structures.ast_nodes import *
 from tests.unit_tests.parser_tests.base_test import ParserTestCase
 
-class ParseDefineCommandTest(ParserTestCase):
+class ParseDefineProcedureTest(ParserTestCase):
     """
     tests the parse_block function
     """
-    def assert_define_command(self, result, value, argc):
+    def assert_define_procedure(self, result, type_, value, argc):
         """
         asserts a define command node
         """
+        self.assertEqual(result.value, type_)
         self.assertEqual(result.children[0].value, value)
         self.assertEqual(len(result.children) - 2, argc)
         self.assertIsInstance(result.children[-1], DBNBlockNode)
     
-    def test_define_command(self):
+    def test_define_procedure_command(self):
         """
-        tests a noarg command defind
+        tests a noarg command define
         """
         tokens = self.make_tokens(
             ('COMMAND', ''),
@@ -28,12 +29,27 @@ class ParseDefineCommandTest(ParserTestCase):
             ('CLOSEBRACE', ''),
             ('NEWLINE', ''),
         )
-        result, result_tokens = self.run_parse(parser.parse_define_command, tokens, expected=DBNCommandDefinitionNode)
-        self.assert_define_command(result, 'Square', 0)
+        result, result_tokens = self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
+        self.assert_define_procedure(result, 'COMMAND', 'Square', 0)
 
-    def test_define_command_with_args(self):
+    def test_define_procedure_number(self):
         """
-        tests a noarg command defind
+        tests a noarg number define
+        """
+        tokens = self.make_tokens(
+            ('NUMBER', ''),
+            ('WORD', 'Square'),
+            ('OPENBRACE', ''),
+            ('NEWLINE', ''),
+            ('CLOSEBRACE', ''),
+            ('NEWLINE', ''),
+        )
+        result, result_tokens = self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
+        self.assert_define_procedure(result, 'NUMBER', 'Square', 0)
+
+    def test_define_procedure_with_args_command(self):
+        """
+        tests a command define with args
         """
         tokens = self.make_tokens(
             ('COMMAND', ''),
@@ -47,10 +63,29 @@ class ParseDefineCommandTest(ParserTestCase):
             ('CLOSEBRACE', ''),
             ('NEWLINE', ''),
         )
-        result, result_tokens = self.run_parse(parser.parse_define_command, tokens, expected=DBNCommandDefinitionNode)
-        self.assert_define_command(result, 'Ploop', 4)
+        result, result_tokens = self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
+        self.assert_define_procedure(result, 'COMMAND', 'Ploop', 4)
+
+    def test_define_procedure_with_args_number(self):
+        """
+        tests a number define with args
+        """
+        tokens = self.make_tokens(
+            ('NUMBER', ''),
+            ('WORD', 'Ploop'),
+            ('WORD', 'X'),
+            ('WORD', 'Y'),
+            ('WORD', 'L'),
+            ('WORD', 'R'),
+            ('OPENBRACE', ''),
+            ('NEWLINE', ''),
+            ('CLOSEBRACE', ''),
+            ('NEWLINE', ''),
+        )
+        result, result_tokens = self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
+        self.assert_define_procedure(result, 'NUMBER', 'Ploop', 4)
         
-    def test_define_command_no_newline(self):
+    def test_define_procedure_no_newline(self):
         """
         tests that an error is thrown without a terminating newline
         """
@@ -66,7 +101,7 @@ class ParseDefineCommandTest(ParserTestCase):
         )
         
         with self.assertRaises(ValueError):
-            self.run_parse(parser.parse_define_command, tokens, expected=DBNCommandDefinitionNode)
+            self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
     
     def test_complain_no_args(self):
         """
@@ -81,7 +116,7 @@ class ParseDefineCommandTest(ParserTestCase):
         )
         
         with self.assertRaises(ValueError):
-            self.run_parse(parser.parse_define_command, tokens, expected=DBNCommandDefinitionNode)
+            self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
         
     def test_complain_non_word_arg(self):
         """
@@ -104,7 +139,7 @@ class ParseDefineCommandTest(ParserTestCase):
         )
         
         with self.assertRaises(ValueError):
-            self.run_parse(parser.parse_define_command, tokens, expected=DBNCommandDefinitionNode)
+            self.run_parse(parser.parse_define_procedure, tokens, expected=DBNProcedureDefinitionNode)
     
 
 
